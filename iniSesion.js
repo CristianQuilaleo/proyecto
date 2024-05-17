375e5d4627d8ecd395484f3b48e96fede1427948
cristian Quilaleo <cristian.quilaleo289@gmail.com>
2024-05-17 18:14:33 -0400

    2do js

--- iniSesion.js
+++ iniSesion.js
@@ -1,25 +1,24 @@
 // Función para iniciar sesión
 function iniciarSesion() {
-    // Obtener el correo electrónico y la contraseña
     var email = document.getElementById('exampleInputEmail1').value;
     var password = document.getElementById('exampleInputPassword1').value;
 
-    // Verificar si los campos están vacíos
     if (!email || !password) {
         alert('Por favor, complete todos los campos.');
         return;
     }
 
-    // Simulación de verificación de credenciales (reemplaza con tu lógica real)
-    // Aquí puedes verificar las credenciales con tu backend
-    if (email === 'usuario@example.com' && password === 'contraseña') {
-        alert('Inicio de sesión exitoso.'); // Muestra un mensaje de éxito
-        // Aquí puedes redirigir al usuario a la página deseada después del inicio de sesión exitoso
+    // Verificar si las credenciales coinciden con los datos almacenados
+    var storedEmail = localStorage.getItem('email');
+    var storedPassword = localStorage.getItem('password');
+
+    if (email === storedEmail && password === storedPassword) {
+        alert('Inicio de sesión exitoso.');
+        // Redirigir al usuario a la página deseada después del inicio de sesión exitoso
         window.location.href = 'home.html';
     } else {
-        // Muestra un mensaje de error y borra los datos de las casillas del formulario
         document.getElementById('mensajeError').innerText = 'Credenciales incorrectas. Por favor, inténtelo de nuevo.';
-        document.getElementById('exampleInputEmail1').value = ''; // Borra el valor del campo de correo electrónico
-        document.getElementById('exampleInputPassword1').value = ''; // Borra el valor del campo de contraseña
+        document.getElementById('exampleInputEmail1').value = '';
+        document.getElementById('exampleInputPassword1').value = '';
     }
 }
